c6f86c8436a4b9e7610d348a409a500328bcbe28
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2020-08-31 04:14:48 -0400

    Downloads all google drive files in directories that have been modified on the current day

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 import pickle
-import os.path
+import os
 import io
+import shutil
+import time
 from googleapiclient.http import MediaIoBaseDownload
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -35,31 +37,59 @@ def main():
 
     service = build('drive', 'v3', credentials=creds)
 
-    # Call the Drive v3 API
+    # Call the Drive v3 API to get Drive folders
     results = service.files().list(
-        pageSize=5, fields="nextPageToken, files(id, name)").execute()
+        q="mimeType='application/vnd.google-apps.folder'", pageSize=1000, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    # Initialize empty dictionary to store folder name to folder ID correspondence
+    folderDict = dict()
+
+    if not items:
+        print('No folders found.')
+    else:
+        print('Folders:')
+        for item in items:
+            folderDict.update({item['id']:item['name']})
+            print("Creating folder ", str(item['name']), " from Google Drive")
+            dir = os.path.join("/","Users","Keith",'Desktop','FALL 2020',item['name'])
+            if not os.path.exists(dir):
+                os.mkdir(dir)
+
+
+    # Call the Drive v3 API to get drive files
+    results = service.files().list(
+        fields="*").execute()
     items = results.get('files', [])
 
     if not items:
         print('No files found.')
     else:
         print('Files:')
+        currentTime = time.strftime('%Y-%m-%dT%H:%M:%SZ')
         for item in items:
-            print("Downloading file ", str(item['name']), " from Google Drive")
-            #print(u'{0} ({1})'.format(item['name'], item['id']))
-            file_id = item['id']
-            request = service.files().get_media(fileId=file_id)
-            fh = io.FileIO(item['name'], 'wb')
-            downloader = MediaIoBaseDownload(fh, request)
-            done = False
-            while done is False:
-                try:
-                    status, done = downloader.next_chunk()
-                    print('Download %d%%.' % int(status.progress() * 100))
-                except:
-                    print('NOT A VALID BINARY FILE')
-                    break
-
+            try:
+                if (item['parents'][0] in folderDict and (str(currentTime)[0:10]==str(item['modifiedTime'][0:10]))):
+                    print(item['modifiedTime'])
+                    print("Downloading file ", str(item['name']), " from Google Drive")
+                    file_id = item['id']
+                    request = service.files().get_media(fileId=file_id)
+                    fh = io.FileIO(item['name'], 'wb')
+                    downloader = MediaIoBaseDownload(fh, request)
+                    done = False
+                    while done is False:
+                        try:
+                            status, done = downloader.next_chunk()
+                            print('Download %d%%.' % int(status.progress() * 100))
+                            oldPath='/Users/Keith/Desktop/GoogleDriveDownloader/'+item['name']
+                            newPath='/Users/Keith/Desktop/Fall 2020/'+folderDict.get(item['parents'][0])+'/'
+                            os.remove(newPath+item['name'])
+                            shutil.move(oldPath,newPath)
+                        except:
+                            continue
+            except:
+                continue
+                
 
 if __name__ == '__main__':
     main()
